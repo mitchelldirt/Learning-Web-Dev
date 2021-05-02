b6fa8158de5405ba17dfba5bef7918346a9c45ac
Mitchell Mudd <mitchellmudd@outlook.com>
2021-05-01 23:17:43 -0400

    Finished up the conditionals intro

--- practice/intro_to_conditionals.js
+++ practice/intro_to_conditionals.js
@@ -200,3 +200,284 @@ function setWeather() {
 // This is the syntax for a Ternary operator (?) -->  ( condition ) ? run this code : run this code instead
 
 // BELOW HERE IS THE CODE FOR THE CALENDAR PROBLEM ON MDN DOCS
+switch (choice) {
+    case "September":
+    case "April":
+    case "June":
+    case "November":
+        days = 30;
+        break;
+    case "February":
+        days = 29;
+        break;
+    default:
+        days = 31;
+        break;
+}
+
+// THIS IS THE SCREEN COLOR PROBLEM ON MDN DOCS:
+// CODE THAT MDN WROTE:
+const select = document.querySelector('select');
+const html = document.querySelector('.output');
+
+select.onchange = function () {
+    const choice = select.value;
+
+    // CODE THAT I WROTE:
+    switch (choice) {
+        case "white":
+            update("white", "black");
+            break;
+        case "black":
+            update("black", "white");
+            break;
+        case "yellow":
+            update("yellow", "white");
+            break;
+        case "purple":
+            update("purple", "white");
+            break;
+        case "psychedelic":
+            update('psychedelic', 'black');
+            break;
+        default:
+            alert("This color isn't available!");
+            break;
+    }
+}
+
+// CODE THAT MDN WROTE FOR ABOVE:
+function update(bgColor, textColor) {
+    html.style.backgroundColor = bgColor;
+    html.style.color = textColor;
+}
+
+// SEASONS RESPONSE CHALLENGE ON MDN:
+let season = 'summer';
+let response;
+
+if (season === 'summer') {
+    response = 'Ahhhhhh, nice and toasy outside!';
+}
+else if (season === 'winter') {
+    response = 'Brrrrr, it sure is cold outside!'
+}
+else {
+    response = 'Sorry, I don\'t recognize that month';
+}
+
+// ANSWERING MACHINE SCORE PROBLEM ON MDN DOCS
+let response;
+let score = 75;
+let machineActive = false;
+
+if (machineActive === false) {
+    response = 'Please turn on the answering machine!';
+}
+else if (machineActive === true && score >= 0 && score <= 19) {
+    response = 'That was a terrible score — total fail!';
+}
+else if (machineActive === true && score >= 20 && score <= 39) {
+    response = 'You know some things, but it\'s a pretty bad score. Needs improvement.';
+}
+else if (machineActive === true && score >= 40 && score <= 69) {
+    response = 'You did a passable job, not bad!';
+}
+else if (machineActive === true && score >= 70 && score <= 89) {
+    response = 'That\'s a great score, you really know your stuff.';
+}
+else if (machineActive === true && score >= 90 && score <= 100) {
+    response = 'What an amazing score! Did you cheat? Are you for real?';
+}
+else if (machineActive === true && score < 0 || score > 100 || null || undefined) {
+    response = 'This is not possible, an error has occurred.';
+}
+else {
+    response = "Invalid!"
+}
+
+// SAME PROBLEM AS ABOVE BUT USING SWITCH STATEMENT
+// THE BELOW IS BAD USE THE NEXT SOLUTION BELOW THAT INSTEAD!
+
+if (machineActive) {
+    switch (score) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+            response = 'That was a terrible score — total fail!';
+            break;
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+        case 25:
+        case 26:
+        case 27:
+        case 28:
+        case 29:
+        case 30:
+        case 31:
+        case 32:
+        case 33:
+        case 34:
+        case 35:
+        case 36:
+        case 37:
+        case 38:
+        case 39:
+            response = 'You know some things, but it\'s a pretty bad score. Needs improvement.';
+            break;
+        case 40:
+        case 41:
+        case 42:
+        case 43:
+        case 44:
+        case 45:
+        case 46:
+        case 47:
+        case 48:
+        case 49:
+        case 50:
+        case 51:
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+        case 56:
+        case 57:
+        case 58:
+        case 59:
+        case 60:
+        case 61:
+        case 62:
+        case 63:
+        case 64:
+        case 65:
+        case 66:
+        case 67:
+        case 68:
+        case 69:
+            response = 'You did a passable job, not bad!';
+            break;
+        case 70:
+        case 71:
+        case 72:
+        case 73:
+        case 74:
+        case 75:
+        case 76:
+        case 77:
+        case 78:
+        case 79:
+        case 80:
+        case 81:
+        case 82:
+        case 83:
+        case 84:
+        case 85:
+        case 86:
+        case 87:
+        case 88:
+        case 89:
+            response = 'That\'s a great score, you really know your stuff.';
+            break;
+        case 90:
+        case 91:
+        case 92:
+        case 93:
+        case 94:
+        case 95:
+        case 96:
+        case 97:
+        case 98:
+        case 99:
+        case 100:
+            response = 'What an amazing score! Did you cheat? Are you for real?';
+            break;
+        default:
+            response = 'This is not possible, an error has ocurred';
+            break;
+
+    }
+} else {
+    response = 'Please turn the answering machine on!';
+}
+
+// THE ABOVE IS A POOR WAY OF DOING THIS USE THE BELOW INSTEAD!
+// YOU COULD ALSO DO the >= THING YOU TRIED BEFORE JUST MAKE SURE THAT SWITCH (true) IS SET!
+if (machineActive) {
+    switch (true) {
+        case score > 100:
+            response = 'This is not possible, an error has ocurred';
+            break;
+        case score >= 90:
+            response = 'What an amazing score! Did you cheat? Are you for real?';
+            break;
+        case score >= 70:
+            response = 'That\'s a great score, you really know your stuff.';
+            break;
+        case score >= 40:
+            response = 'You did a passable job, not bad!';
+            break;
+        case score >= 20:
+            response = 'You know some things, but it\'s a pretty bad score. Needs improvement.';
+            break;
+        case score >= 0:
+            response = 'That was a terrible score — total fail!';
+            break;
+        case score < 0:
+            response = 'This is not possible, an error has ocurred';
+            break;
+    }
+
+    let pwd = 'cheese';
+    let machineResult;
+    let pwdResult;
+
+    if (machineActive) {
+        machineResult = 'The machine is turned on, please enter a password';
+        pwd === 'cheese' ? pwdResult = 'You have logged in successfully!' : pwdResult = 'Wrong password!';
+    }
+    else {
+        machineResult = 'The machine is currently inactive';
+    }
+
+    // Javascript.info problem below
+    let jsName = prompt('What is the real name of javascript?')
+    alert(jsName === 'ECMAScript' ? 'That\'s right!' : 'You don\'t know? ECMAScript!');
+
+    // Javascript.info problem below
+    result = prompt('Enter a number!');
+
+    if (result === 0) {
+        result = 0;
+    } else if (result > 0) {
+        result = 1;
+    } else {
+        result = -1;
+    }
+
+    alert(result);
+
+    // Javascript.info problem below
+    result = (a + b < 4) ? 'Below' : 'Over';
+
+// Javascript.info problem below
