47f72c549b1daf0cfb869efab2abd3c9910fcd2c
MitchellMudd <mitchellmudd@outlook.com>
2021-05-03 13:05:10 -0400

    Learned more about functions. Feeling more confident

--- practice/intro_to_functions.js
+++ practice/intro_to_functions.js
@@ -36,4 +36,94 @@ function random(number) {
     return Math.floor(Math.random() * number);
 }
 
+// Example of a function without a set return value below:
+function favoriteAnimal(animal) {
+    console.log(animal + " is my favorite animal!")
+  }
+  
+  favoriteAnimal('Goat')
 
+// JASVASCRIPT.INFO NOTES BELOW:
+
+// Initial function example
+function showMessage() {
+    alert( 'Hello Everyone!' );
+}
+
+showMessage();
+
+// Below shows local variable example
+function showMessage2() {
+    let message = 'Hello, I\'m Javascript!'; // local variable message, only applies to this function as it's local
+
+    alert(message);
+}
+
+showMessage2(); // Will print 'Hello, I'm Javascript!'
+
+alert(message); // Won't work because it is ot of the scope of the function.
+
+// You can use global variables in functions but local variables stay inside the function... Example below:
+let userName = 'John'
+
+function showMessage3() {
+    let userName = 'Bob'; // Usually would cause an error but this is a local variable
+
+    let message = 'Hello, ' + userName; // Will be Bob
+    alert(message);
+}
+
+// Function use the local variable not the global one because the local variable shadows the global variable
+showMessage3(); // Bob
+
+alert(userName); // John
+
+
+// Below is an example of parameters in functions:
+function showMessage4(from, text) {
+    alert(from + ': ' + text);
+}
+
+showMessage4('Ann', 'Hello!');
+showMessage4('Ann', 'What\'s up?');
+
+// Here's another way to do the above function
+// text has the default value 'No text given' if you don't anything
+function showMessage5(from, text = 'No text given') {
+
+    from = '*' + from + '*'; // make "from" look nicer
+  
+    alert( from + ': ' + text );
+  }
+  
+  let from = "Ann";
+  
+  showMessage5(from, "Hello"); // *Ann*: Hello
+  
+  // the value of "from" is the same, the function modified a local copy
+  alert( from ); // Ann
+
+  // You can do more complex default values such as anotherFunction() like shown below:
+  function showMessage6(from, text = anotherFunction()) {
+    // Another way to declare defauly values  
+    if (text === undefined) {
+          text ='Empty Message';
+      }
+
+      // ANOTHER way to do default values
+      text = text || 'Empty Message';
+
+
+    // anotherFunction() only executed if no text given
+    // its result becomes the value of text
+  }
+
+// Return values,,, again
+  function sum(a, b) {
+      return a + b;
+  }
+
+  let result = sum(1, 2);
+  alert(result); // Will output 3
+
+  // YOU ARE AT THE RETURNING A VALUE SECTION OF THE JSINFO ARTICLE!
