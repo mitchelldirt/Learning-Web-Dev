aa08b26ff7d69ba8d7d3434b033abce178342897
Mitchell <mitchellmudd@outlook.com>
2021-06-16 21:31:43 -0400

    Completed some JS exercises

--- practice/FCC/basicAlgorithms.js
+++ practice/FCC/basicAlgorithms.js
@@ -7,7 +7,7 @@ function factorialize(num) {
     return result;
 }
 
-factorialize(5);
+factorialize(5); // 120
 
 //Find the longest word in a string
 function findLongestWordLength(str) {
@@ -23,7 +23,7 @@ function findLongestWordLength(str) {
     return characterCount;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+findLongestWordLength("The quick brown fox jumped over the lazy dog"); // 6
 
 // Largest Numbers in Arrays
 function largestOfFour(arr) {
@@ -44,4 +44,89 @@ function largestOfFour(arr) {
     return finalArray;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); // [5, 27, 39, 1001]
+
+// Confirm the ending
+function confirmEnding(str, target) {
+    let amountToIndex = target.length * -1;
+    let endOfString = str.slice(amountToIndex);
+    console.log(endOfString);
+    if (endOfString === target) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+confirmEnding("Bastian", "n"); // n
+
+// Repeat a string
+function repeatStringNumTimes(str, num) {
+    let newStr = "";
+    for (let i = 0; i < num; i++) {
+        newStr += str;
+    }
+    return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
+
+// Truncate a string
+function truncateString(str, num) {
+    if (str.length <= num) {
+        return str;
+    } else {
+        return str.slice(0, num) + "..."
+    }
+    return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// Does element pass the function?
+function findElement(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) === false) {
+            continue;
+        } else {
+            return arr[i];
+        }
+    }
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0); // 2
+
+// Is it a bool?
+function booWho(bool) {
+    if (bool === true || bool === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+booWho(null);
+
+// Uppercase each work in a string
+function titleCase(str) {
+    let lowerCaseStr = str.toLowerCase();
+    let strArray = lowerCaseStr.split(" ");
+    for (let i = 0; i < strArray.length; i++) {
+        strArray[i] = strArray[i].charAt(0).toUpperCase() + strArray[i].slice(1);
+    }
+    return strArray.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
+
+// Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+    let result = [...arr2]
+    for (let i = 0; i < arr1.length; i++) {
+        result.splice(n, 0, arr1[i])
+        n += 1;
+    }
+    return result;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
