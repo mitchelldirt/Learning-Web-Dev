0f39f1e6fb4389ff95f13e0fcab0260410fb000e
Mitchell Mudd <mitchellmudd@outlook.com>
2021-07-27 05:46:50 -0400

    Whoops forgot to push this to github forever ago

--- practice/odin_project_lessons/factoryFunctions.js
+++ practice/odin_project_lessons/factoryFunctions.js
@@ -84,3 +84,22 @@ const jeff = Nerd('jeff')
 
 jeff.sayName() //my name is jeff
 jeff.doSomethingNerdy() // nerd stuff
+
+
+// This is an example of a module. It's like a factory function but instead it's wrapped in an IIFE.
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+        add,
+        sub,
+        mul,
+        div,
+    };
+})();
+
+console.log(calculator.add(3, 5)) // 8
+console.log(calculator.sub(6, 2)) // 4
+console.log(calculator.mul(14, 5534)) // 77476
