b16fdc96bb4d250f7c65048636c3607a69304003
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-08 13:51:31 -0500

    Zip code validation is working now :)

--- practice/odin_project_lessons/formValidation/app.js
+++ practice/odin_project_lessons/formValidation/app.js
@@ -1,7 +1,6 @@
 // There are many ways to pick a DOM node; here we get the form itself and the email
 // input box, as well as the span element into which we will place the error message.
-const form  = document.getElementsByTagName('form')[0];
-
+const form = document.getElementsByTagName('form')[0];
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
 
@@ -23,29 +22,54 @@ email.addEventListener('input', function (event) {
 form.addEventListener('submit', function (event) {
   // if the email field is valid, we let the form submit
 
-  if(!email.validity.valid) {
+  if (!email.validity.valid) {
     // If it isn't, we display an appropriate error message
     showError();
     // Then we prevent the form from being sent by canceling the event
     event.preventDefault();
+  } else if (!zipCodeBox.validity.valid) {
+    showZipCodeError()
+    event.preventDefault()
   }
 });
 
 function showError() {
-  if(email.validity.valueMissing) {
+  if (email.validity.valueMissing) {
     // If the field is empty,
     // display the following error message.
     emailError.textContent = 'You need to enter an e-mail address.';
-  } else if(email.validity.typeMismatch) {
+  } else if (email.validity.typeMismatch) {
     // If the field doesn't contain an email address,
     // display the following error message.
     emailError.textContent = 'Entered value needs to be an e-mail address.';
-  } else if(email.validity.tooShort) {
+  } else if (email.validity.tooShort) {
     // If the data is too short,
     // display the following error message.
-    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
   }
 
   // Set the styling appropriately
   emailError.className = 'error active';
+}
+
+const zipCodeBox = document.getElementById("zip");
+const errorZip = document.getElementById("errorZip")
+
+zipCodeBox.addEventListener("input", function (event) {
+  if (zipCodeBox.validity.valid) {
+    errorZip.textContent = '';
+    errorZip.className = "errorZip";
+  } else {
+    showZipCodeError();
+  }
+})
+
+function showZipCodeError() {
+  if (zipCodeBox.validity.patternMismatch) {
+    errorZip.textContent = "Only numbers please :)"
+  } else if (zipCodeBox.validity.tooShort) {
+    errorZip.textContent = "Minimum 4 numbers"
+  } else if (zipCodeBox.validity.tooLong) {
+    errorZip.textContent = "Maximum 6 numbers";
+  }
 }
