800bd65eb492fee76107ffe2678f33109109cbd2
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-16 09:55:08 -0500

    Created an API search engine lol

---
+++ practice/odin_project_lessons/APIs/app.js
@@ -0,0 +1,24 @@
+const img = document.querySelector('img');
+const btn = document.querySelector('button');
+const input = document.querySelector('input')
+let query = "pokemon"
+btn.onclick = (ev) => {
+    if (input.value != "") {
+        query = input.value;
+    }
+    ev.preventDefault();
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=10YMYw3C0R31pNY2yK4Wf3YsAPMNP5yp&s=${query}`, {mode: "cors"})
+    .then (function(response) {
+        return response.json();
+    })
+    .then (function(response) {
+        img.src = response.data.images.original.url;
+        let span = document.querySelector('span').classList.add("hidden");
+    })
+    .catch (function(err) {
+        let span = document.querySelector('span').classList.remove("hidden");
+        img.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/oh-no-face-3472821-2906934.png"
+    })
+}
+
+btn.click();
