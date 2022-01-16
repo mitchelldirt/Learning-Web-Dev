f212de68df2211b2ade788a258070e29bd0c0c32
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-16 11:33:51 -0500

    Updated the ui a bit to make it look like I actually put effort into this

--- practice/odin_project_lessons/APIs/app.js
+++ practice/odin_project_lessons/APIs/app.js
@@ -7,18 +7,18 @@ btn.onclick = (ev) => {
         query = input.value;
     }
     ev.preventDefault();
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=10YMYw3C0R31pNY2yK4Wf3YsAPMNP5yp&s=${query}`, {mode: "cors"})
-    .then (function(response) {
-        return response.json();
-    })
-    .then (function(response) {
-        img.src = response.data.images.original.url;
-        let span = document.querySelector('span').classList.add("hidden");
-    })
-    .catch (function(err) {
-        let span = document.querySelector('span').classList.remove("hidden");
-        img.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/oh-no-face-3472821-2906934.png"
-    })
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=10YMYw3C0R31pNY2yK4Wf3YsAPMNP5yp&s=${query}`, { mode: "cors" })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            img.src = response.data.images.original.url;
+            let span = document.querySelector('span').classList.add("hidden");
+        })
+        .catch(function (err) {
+            let span = document.querySelector('span').classList.remove("hidden");
+            img.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/oh-no-face-3472821-2906934.png"
+        })
 }
 
 btn.click();
