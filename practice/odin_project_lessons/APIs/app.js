e91856767302e5884742956e83a5223aaabeea1d
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-17 21:12:06 -0500

    Changed the promise based function to async await

--- practice/odin_project_lessons/APIs/app.js
+++ practice/odin_project_lessons/APIs/app.js
@@ -2,23 +2,23 @@ const img = document.querySelector('img');
 const btn = document.querySelector('button');
 const input = document.querySelector('input')
 let query = "pokemon"
-btn.onclick = (ev) => {
+btn.onclick = replaceGif;
+
+async function replaceGif() {
     if (input.value != "") {
         query = input.value;
+    };
+
+    let queryResult = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=10YMYw3C0R31pNY2yK4Wf3YsAPMNP5yp&s=${query}`, { mode: "cors" });
+    let url = await queryResult.json();
+
+    try {
+        img.src = url.data.images.original.url;
+        let span = document.querySelector('span').classList.add("hidden");
+    } catch {
+        let span = document.querySelector('span').classList.remove("hidden");
+        img.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/oh-no-face-3472821-2906934.png"
     }
-    ev.preventDefault();
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=10YMYw3C0R31pNY2yK4Wf3YsAPMNP5yp&s=${query}`, { mode: "cors" })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            img.src = response.data.images.original.url;
-            let span = document.querySelector('span').classList.add("hidden");
-        })
-        .catch(function (err) {
-            let span = document.querySelector('span').classList.remove("hidden");
-            img.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/oh-no-face-3472821-2906934.png"
-        })
 }
 
 btn.click();
