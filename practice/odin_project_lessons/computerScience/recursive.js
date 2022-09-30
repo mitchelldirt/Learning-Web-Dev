7ca3f17a25211e2a004148fc60e819135e899654
mitchelldirt <mitchellmudd@outlook.com>
2022-09-30 09:28:37 -0400

    Started exercises

---
+++ practice/odin_project_lessons/computerScience/recursive.js
@@ -0,0 +1,20 @@
+//Iterative version using for loop
+function sumIterative(inputNumber) {
+    let result = 0;
+    for (let i = 0; i <= inputNumber; i++) {
+        result += i
+    }
+    return result
+}
+
+console.log(sumIterative(100))
+
+//Recursive version of sum
+function sumRecursive(inputNumber) {
+
+}
+
+//Arithmetic version of sum
+function sumArithmetic() {
+    
+}
