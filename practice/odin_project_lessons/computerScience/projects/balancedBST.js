74f7b407ed58abdc4b2484eaee237de6cd6ebcb7
mitchelldirt <mitchellmudd@outlook.com>
2022-10-06 08:31:18 -0400

    Wrote out the psuedocode for balance BST

--- practice/odin_project_lessons/computerScience/projects/balancedBST.js
+++ practice/odin_project_lessons/computerScience/projects/balancedBST.js
@@ -12,7 +12,7 @@ class Tree {
     root = null
 
     constructor(array) {
-        this.array = array;
+        this.array = cleanArray(array);
     }
 
     cleanArray() {
@@ -24,8 +24,18 @@ class Tree {
             index) => this.array.indexOf(item) === index);
     }
 
-    buildTree() {
+    buildTree(array) {
         //TODO: write function haha
+
+        // STEP 1: Set the root of the tree (middle of the array)
+
+        // STEP 2: Set the items to the left of the root as left child and right of the root as right child
+
+        // STEP 3: Recursively do the same for the left and right half
+
+        // STEP 4: base case = start > end
+
+        // use article linked from odin project for reference
     }
 }
 
